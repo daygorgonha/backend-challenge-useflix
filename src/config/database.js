2f1430e641439f48d92263b49f7cabce5d87bafd
Dayane B X Gorgonha <dayanebismark18@gmail.com>
2022-06-03 20:34:52 -0300

    feat: initial project configuration

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'useflix',
+  password: 'useflix',
+  database: 'useflix',
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
