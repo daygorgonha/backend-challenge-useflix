2a9a2bf993c9b72367d7dd48dfdea1c84d880408
Dayane B X Gorgonha <dayanebismark18@gmail.com>
2022-06-04 12:47:18 -0300

    feat: added movie controller

---
+++ src/controllers/MovieController.js
@@ -0,0 +1,39 @@
+const Movie = require('../models/Movie');
+
+module.exports = { 
+  async index(req, res) {
+    const movies = await Movie.findAll();
+
+    return res.json(movies);
+  },
+
+
+  async show(req, res) {
+    const { id } = req.params;
+
+    const movie = await Movie.findByPk(id);
+
+    return res.json(movie);
+  },
+
+
+  async store(req, res) {
+    const { category_id, name, classification } = req.body;
+
+    const movie = await Movie.create({ 
+      category_id, 
+      name, 
+      classification,
+    });
+
+    return res.json(movie);
+  },
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    await Movie.destroy({ where: { id } });
+
+    return res.send();
+  }
+};
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const CategoryController = require('./controllers/CategoryController');
+const MovieController = require('./controllers/MovieController');
 
 const routes = express.Router();
 
@@ -7,6 +8,11 @@ routes.get('/categories', CategoryController.index);
 routes.get('/categories/:id', CategoryController.show);
 routes.post('/categories', CategoryController.store);
 routes.delete('/categories/:id', CategoryController.delete);
+
+routes.get('/movies', MovieController.index);
+routes.get('/movies/:id', MovieController.show);
+routes.post('/movies', MovieController.store);
+routes.delete('/movies/:id', MovieController.delete);
 
 
 module.exports = routes;
