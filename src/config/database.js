9e87576f4af6f78de664c291080bdf34175f5d5f
Dayane B X Gorgonha <dayanebismark18@gmail.com>
2022-06-09 12:05:20 -0300

    fix: adjusted process env

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
   protocol: 'postgres',
-  host: process.env.DATABASE_URL,
-  username: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DB_URL,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: process.env.DATABASE,
   dialectOptions: {
     ssl: parseInt(process.env.ENABLE_SSL) === 1 ? true : false,
