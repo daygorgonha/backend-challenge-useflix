a13ae633f9a19564ba4fe67fc2084df13564ee94
Dayane B X Gorgonha <dayanebismark18@gmail.com>
2022-06-09 14:48:45 -0300

    refactor: adjusted cors origins all request

--- src/server.js
+++ src/server.js
@@ -8,7 +8,9 @@ require('./database');
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
