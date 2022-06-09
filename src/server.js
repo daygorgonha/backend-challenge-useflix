72cbe0a4dddaa0e4b92bd0d516f19973625333d8
Dayane B X Gorgonha <dayanebismark18@gmail.com>
2022-06-09 12:43:36 -0300

    feat: added cors

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 
 require('./database');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
-  console.log("Server is running!")
+  console.log('Server is running!')
 });
